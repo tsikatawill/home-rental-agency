21ec2c623f5c7062bd4a34aabd9c9cb2dff7f818
William M. Tsikata <tsikatawill@gmail.com>
2023-01-05 21:06:29 +0000

    feat: add header

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components";
+
+export const Header = () => {
+  return (
+    <header className="h-[790px] bg-[url(/images/header-bg.jpg)] bg-cover bg-no-repeat bg-center">
+      <Navbar />
+
+      {/* Hero */}
+    </header>
+  );
+};
