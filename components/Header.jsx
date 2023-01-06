efaf7782c704a46885df8b6ac65b5f33a6a03467
William M. Tsikata <tsikatawill@gmail.com>
2023-01-06 02:37:57 +0000

    style: add gradient to header bg

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,16 @@
-import { Navbar } from "../components";
+import { Hero, Navbar } from "../components";
 
 export const Header = () => {
   return (
-    <header className="h-[790px] bg-[url(/images/header-bg.jpg)] bg-cover bg-no-repeat bg-center">
+    <header
+      className="h-[700px] -tracking-[1px]"
+      style={{
+        background:
+          "linear-gradient(200.44deg, rgba(0, 0, 0, 0.0001) 16.41%, rgba(0, 0, 0, 0.51) 77.16%), url(/images/header-bg.jpg)",
+      }}
+    >
       <Navbar />
-
-      {/* Hero */}
+      <Hero />
     </header>
   );
 };
