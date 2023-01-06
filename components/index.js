69093c89b7fd7cd798429611161d8804c73ae760
William M. Tsikata <tsikatawill@gmail.com>
2023-01-06 02:42:01 +0000

    chore: export components

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 export * from "./AnimatedToggle";
 export * from "./Container";
 export * from "./Header";
+export * from "./Hero";
 export * from "./Logo";
+export * from "./Map";
 export * from "./Navbar";
