bf229133e4c7d06f75b86c757a8d87264b7b7a91
William M. Tsikata <tsikatawill@gmail.com>
2023-01-06 07:15:20 +0000

    style: fxed bg attachment + height mod

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,11 @@ import { Hero, Navbar } from "../components";
 export const Header = () => {
   return (
     <header
-      className="h-[700px] -tracking-[1px]"
+      className="h-fit pb-20 sm:pb-0 sm:h-[700px] -tracking-[1px]"
       style={{
         background:
-          "linear-gradient(200.44deg, rgba(0, 0, 0, 0.0001) 16.41%, rgba(0, 0, 0, 0.51) 77.16%), url(/images/header-bg.jpg)",
+          "linear-gradient(200.44deg, rgba(0, 0, 0, 0.0001) 16.41%, rgba(0, 0, 0, 0.51) 77.16%), url(/images/header-bg.jpg) no-repeat center center/cover",
+        backgroundAttachment: "fixed",
       }}
     >
       <Navbar />
