1ba31c2155195ba069dfccb74aaf62f872b5a56e
William M. Tsikata <tsikatawill@gmail.com>
2023-01-06 07:15:56 +0000

    chore: tailor colors ,ore to figma design

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,5 @@
 import { Container, Map } from "../components";
-import { scaleUp, slideY } from "../animations";
+import { scaleUp, slideX, slideY } from "../animations";
 
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -39,12 +39,12 @@ export const Hero = () => {
   };
 
   return (
-    <section className="h-full">
-      <Container extraClasses="h-full">
-        <div className="h-full grid grid-cols-2 place-content-center items-center text-white">
+    <section className="h-full mt-20">
+      <Container>
+        <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 place-content-center items-center text-white">
           <motion.h1
-            variants={slideY()}
-            transition={{ type: "spring", stiffness: 50, damping: 10 }}
+            variants={slideX()}
+            transition={{ duration: 0.3 }}
             initial="initial"
             animate="animate"
             className="capitalize font-bold text-[52px] leading-[70px]"
@@ -58,46 +58,47 @@ export const Hero = () => {
             whileInView="animate"
             viewport={{ once: true }}
             transition={{ staggerChildren: 0.1, when: "beforeChildren" }}
-            className="justify-self-end"
+            className="justify-self-center  sm:justify-self-end"
           >
             <motion.div
               variants={scaleUp}
-              className="rounded-2xl border border-[#f9f9f9] overflow-hidden w-[408px] h-[340px] mb-5 mx-auto bg-slate-500"
+              className="rounded-2xl border border-[#f9f9f9] overflow-hidden max-w-[408px] h-[340px] mb-5 mx-auto bg-slate-500"
             >
               <Map coordinates={coordinates} />
             </motion.div>
 
             <motion.div
               variants={scaleUp}
-              className="bg-white text-black rounded-2xl h-[88px] p-4 w-[408px]"
+              className="bg-white text-black rounded-[10px] h-[88px] p-4 max-w-[408px] border border-[rbga(0,0,0,0.2)]"
             >
               <form
-                className="border border-[#f9f9f9] rounded-md overflow-hidden flex gap-2"
+                className="border border-[#e6e6e6] rounded-md overflow-hidden flex gap-2 bg-[#f9f9f9]"
                 onSubmit={(e) => handleSubmit(e)}
               >
                 <select
                   name=""
                   id=""
-                  className="focus:outline-0 h-14 p-2 border-r border-r-[#f9f9f9]"
+                  className="focus:outline-0 h-14 p-4 pr-0 mr-6 bg-transparent cursor-pointer"
                 >
                   <option value="all">All types</option>
                   <option value="apartment">Apartment</option>
                   <option value="2-bed">2-bedroom</option>
                   <option value="3-bed">3-bedroom</option>
                 </select>
-                <div className="flex">
+                <div className="flex border-l border-l-[#e6e6e6]">
                   <input
                     type="text"
                     placeholder="Location"
                     value={submitting ? "Loading map..." : location}
                     onChange={(e) => setLocation(e.target.value)}
-                    className="h-14 w-full p-2 focus:outline-0 transition-all duration-200"
+                    className="py-[15px] px-[23px] w-full p-2 focus:outline-0 bg-transparent transition-all duration-200"
                     required
+                    spellCheck="false"
                   />
                   <button
                     type="submit"
                     disabled={submitting}
-                    className="bg-blue-500 disabled:bg-slate-600 disabled:cursor-not-allowed h-14 w-20 grid place-content-center"
+                    className="bg-[#23A6F0] disabled:bg-slate-600 disabled:cursor-not-allowed h-14 w-20 grid place-content-center"
                   >
                     <Image
                       src="/icons/search.svg"
