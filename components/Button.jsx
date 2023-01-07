285a9578e2619e754994eb43b9f1e525a1dd49e2
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 18:04:42 +0000

    chore: add extraClasses prop

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,13 +1,13 @@
 import { click } from "../animations";
 import { motion } from "framer-motion";
 
-export const Button = ({ children, rounded }) => {
+export const Button = ({ children, rounded, extraClasses }) => {
   return (
     <motion.button
       variants={click}
       initial="initial"
       whileTap="animate"
-      className={`h-[65px] px-10 grid place-content-center font-medium text-lg bg-orange-500 text-white hover:bg-orange-600 transition-all duration-150 ease-in ${
+      className={`h-[65px] px-10 ${extraClasses} grid place-content-center font-medium text-lg bg-orange-500 text-white hover:bg-orange-600 transition-all duration-150 ease-in ${
         rounded ? "rounded-xl" : "rounded-md"
       }`}
     >
