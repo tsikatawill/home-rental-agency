731309d4adc9bfdf543ce9457d5f6d4e23f4afac
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 20:39:49 +0000

    chore: remove commented code

--- components/TestimonialSlider.jsx
+++ components/TestimonialSlider.jsx
@@ -59,44 +59,12 @@ export const TestimonialSlider = ({
           </motion.div>
         ) : null;
       })}
-      {/* {selected && (
-        <motion.div
-          variants={slideX()}
-          initial="initial"
-          whileInView="animate"
-          className="relative pl-5"
-        >
-          <Image
-            className="absolute left-0 -top-7"
-            src="/icons/quotes.svg"
-            width={60}
-            height={60}
-            alt="quotes.svg"
-          />
-          <p className="leading-[36px]">{selected.text}</p>
 
-          <div className="flex gap-5 items-center mt-7">
-            <div className="border-2 border-orange-500 w-[60] h-[60] rounded-full overflow-hidden">
-              <Image
-                className="w-full h-full object-cover"
-                src={selected.image}
-                width={60}
-                height={60}
-              />
-            </div>
-
-            <div>
-              <p className="text-orange-500 font-bold text-lg">Some name</p>
-              <p className="font-bold">Some name</p>
-            </div>
-          </div>
-        </motion.div>
-      )} */}
       <div className="flex gap-3 mt-10 md:mt-20 pl-5">
         {testimonies.map((item, idx) => (
           <div
             className={`p-2 my-2 h-1 w-1 cursor-pointer ${
-              item.user === selected.user ? "bg-orange-500" : "bg-black"
+              item.user === selected.user ? "bg-orange-500" : "bg-slate-300"
             }  rounded-full`}
             key={idx}
             onClick={() => {
