312618d25b4d25eac50ff0d437b52f4c91c0eb49
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 18:05:06 +0000

    feat: create drag n drop zone

---
+++ components/FormElements/DnD.jsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect } from "react";
+
+import { useDropzone } from "react-dropzone";
+
+export const DnD = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    alert("Files accepted");
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone();
+
+  return (
+    <div
+      {...getRootProps()}
+      className={`w-full h-[100px] p-5 cursor-pointer text-center ${
+        isDragActive ? "bg-orange-50" : "bg-[#F9F9F9]"
+      } font-700 focus:outline-0 rounded-md bg-[rgba(244, 81, 30, 0.01)] border border-dashed border-orange-500 flex justify-center items-center`}
+    >
+      <input
+        {...getInputProps()}
+        type="file"
+        accept="image/png, image/jpg, image/jpeg"
+      />
+      {isDragActive ? (
+        <p>Drop files here ...</p>
+      ) : (
+        <p>
+          Drag your images here, or{" "}
+          <span className="text-orange-500">click to browse</span>
+        </p>
+      )}
+    </div>
+  );
+};
