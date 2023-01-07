c5bd86bf0cebbd3e153150df718aaa23096a5f6e
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 20:29:57 +0000

    style: style dark mode

--- components/FormElements/Select.jsx
+++ components/FormElements/Select.jsx
@@ -1,7 +1,7 @@
 export const Select = ({ children, ...rest }) => {
   return (
     <select
-      className="w-full h-[50px] px-5 focus:outline-0 rounded-md bg-[#F9F9F9] border border-[#E6E6E6]"
+      className="w-full h-[50px] px-5 focus:outline-0 rounded-md bg-[#F9F9F9] border border-[#E6E6E6]  dark:border-slate-500 dark:bg-[rgba(0,0,0,0.5)]"
       {...rest}
     >
       {children}
