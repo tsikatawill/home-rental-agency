e8e12c134480cd870e271ab555afd5d90859bab9
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 13:55:11 +0000

    style: modify text alignment for dff screens

--- components/CostCard.jsx
+++ components/CostCard.jsx
@@ -6,7 +6,7 @@ export const CostCard = ({ icon, text }) => {
   return (
     <motion.div
       variants={scaleUp}
-      className="flex-shrink-0 w-[200px] h-fit space-y-5 py-5"
+      className="flex-shrink-0 w-[200px] h-fit gap-5 py-5 flex flex-col items-center md:items-start"
     >
       <div
         className="bg-white h-[77px] w-[77px] grid place-content-center rounded-[10px]"
@@ -14,7 +14,9 @@ export const CostCard = ({ icon, text }) => {
       >
         <Image src={icon} width={45} height={45} alt="icon.svg" />
       </div>
-      <p className="font-semibold leading-[35px] text-2xl capitalize">{text}</p>
+      <p className="font-semibold leading-[35px] text-2xl capitalize text-center md:text-left">
+        {text}
+      </p>
     </motion.div>
   );
 };
