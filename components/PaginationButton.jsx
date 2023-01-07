9ae45ab23e6b0748f83100c5c408bebd37a8a1f7
William M. Tsikata <tsikatawill@gmail.com>
2023-01-07 22:29:34 +0000

    chore: hamdle dark mode styles

--- components/PaginationButton.jsx
+++ components/PaginationButton.jsx
@@ -11,7 +11,7 @@ export const PaginationButton = ({
       className={`p-5 border border-[#bdbdbd] disabled:bg-[#F3F3F3] disabled:text-[#bdbdbd] disabled:cursor-not-allowed  ${
         current
           ? "bg-orange-500 text-white"
-          : "text-orange-500 dark:text-white dark:bg-slate-500 bg-white"
+          : "text-orange-500 dark:text-white dark:disabled:text-slate-500 dark:bg-[rgba(0,0,0,0.5)] bg-white"
       } `}
       disabled={disabled}
     >
